40705e17da8ad2c0c80aa192ac334d329346adb4
ZMLQQQ <91002313+ZMLQQQ@users.noreply.github.com>
2022-12-12 01:00:48 +0800

    Update teacherController.java

--- Teacher/src/main/java/com/example/teacher/controller/teacherController.java
+++ Teacher/src/main/java/com/example/teacher/controller/teacherController.java
@@ -30,7 +30,7 @@ public class teacherController {
     @PostMapping ("/teacher/login")
     public ResponseEntity<Object> teacherLogin(@RequestBody LoginDTO loginDTO){
         // 将学生的id存入token中，作为以后访问接口的凭证。
-        String token = jwtConfig.createToken(loginDTO.getTeacherId());
+        String token = jwtConfig.createToken(loginDTO.getTeacherId(),"老师");
         // 根据用户名密码获取学生具体信息。
         Teacher teacher = iTeacherService.login(loginDTO.getTeacherId(),loginDTO.getPassword());
         // 将具体信息和token一起返回给前端。
